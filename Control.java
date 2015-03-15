dd000902ef1d4b77ddd19058f457c92ad232122e
JohnnySimmonds <simmondsjohnny@gmail.com>
2015-03-15 15:38:51 -0600

    Quick idea on how the driver and passenger buttons should work
    We probably should have it so that in the model if a person is a driver
    passenger is set to false and vice versa

---
+++ Control.java
@@ -0,0 +1,19 @@
+package easyRideControl;
+
+public class Control {
+/*
+we should have it so that if driver is true passenger has to be set to false in the model! and vice versa
+ */
+	boolean driverButton(String userName)
+	{
+		boolean driver = true;
+		return driver;
+	}
+	
+	boolean passengerButton(String userName)
+	{
+		boolean passenger = true;
+		return passenger;
+	}
+	
+}
