123fb67de1d7dde0074540f6a8c4f222e6b0019f
JohnnySimmonds <simmondsjohnny@gmail.com>
2015-03-16 12:32:18 -0600

    Changed it to match your code a little better
    Hope this makes more sense now.

--- Control.java
+++ Control.java
@@ -4,16 +4,14 @@ public class Control {
 /*
 we should have it so that if driver is true passenger has to be set to false in the model! and vice versa
  */
-	boolean driverButton(String userName)
+	public void driverButton(User userName)
 	{
-		boolean driver = true;
-		return driver;
+		userName.setDriver();
 	}
 	
-	boolean passengerButton(String userName)
+	public void passengerButton(User userName)
 	{
-		boolean passenger = true;
-		return passenger;
+		userName.setPassenger();
 	}
 	
 }
