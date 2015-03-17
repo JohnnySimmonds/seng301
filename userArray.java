551dba0ddbaea05b2ace662e0813530acd40ba57
echamman <e.hamman@telus.net>
2015-03-17 10:11:17 -0600

    redid adding new users
    and ID changes

--- userArray.java
+++ userArray.java
@@ -6,30 +6,11 @@ public class userArray{
 
    
    public void addUser(user newUser){
-      Boolean flag = False;
-      int counter = 0;
-      int spot = 0;
       int length = 0;
    
-      if(uArray.length == 0)
-         uArray[0] = newUser;
-      else{
-         while(flag == False){
-            if(uArray[counter] == null)                        //check if the spot is empty
-               uArray[counter] = newUser;
-            else if(uArray[counter].getID() < newUser.getID())    //check if the user's ID is less than the newUser's ID
-               counter++;
-            else{
-               spot = uArray.length - counter;                    //number of items from the end to the spot
-               length = uArray.length;                
-               for(int i = length; i > spot; i--){                //moving each item up until it gets to the spot that the newUser should be at
-                  uArray[i] = uArray[i + 1];       
-               }
-               uArray[spot] = newUser;                            //puts the new user at said spot
-               flag = True;
-            }  
-         }
-      }
+      length = uArray.length();                     //find spot to put new user in
+      uArray[length] = newUser;                     //Put user in
+      newUser.setID(length);                        //set ID
    }
    
    public user findUser(int id){
@@ -43,6 +24,7 @@ public class userArray{
       uArray[id] = null;                                 //remove user from Array
       while(uArray[counter] != null){                //Move all users after removed user down a slot
          uArray[counter] = uArray[counter + 1];
+         uArray[counter+1].setID(counter);              //set new user's ID's
          counter++;
       }
    }
